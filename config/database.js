7656cc07019a11e1f9cb0491823da5cd7784d035
Erwin Egli <erwin.egli@videojet.com>
2024-08-15 00:30:40 +0200

    Update Sequelize configuration for PostgreSQL

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-
 const { Sequelize } = require('sequelize');
 
+// Hier konfigurieren wir die Datenbankverbindung
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+  dialect: 'postgres',  // Stelle sicher, dass der Dialekt auf 'postgres' gesetzt ist
+  port: process.env.DB_PORT || 5432,  // Der Standardport für PostgreSQL ist 5432
 });
 
 module.exports = sequelize;
